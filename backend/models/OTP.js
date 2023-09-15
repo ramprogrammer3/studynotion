1e43b8f0ac94573f728edae4853603fc9dc2f1ad
ram kumar <rammaniyari@gmail.com>
2023-09-15 16:59:53 +0530

    send verification function created

--- backend/models/OTP.js
+++ backend/models/OTP.js
@@ -1,5 +1,6 @@
 
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
 
@@ -16,9 +17,23 @@ const OTPSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now(),
-        expires : 5*60,
+        expires: 5 * 60,
     }
 
 })
+
+// a function to send mail
+
+async function sendVerificationEmail(email, otp) {
+    try {
+
+        const mailResponse = await mailSender(email, "Verification email from studynotion", otp);
+        console.log("Email send successfully", mailResponse);
+
+    } catch (error) {
+        console.log("Error occured while sending mail : ", error);
+        throw error;
+    }
+}
 
 module.exports = mongoose.model("OTP", OTPSchema);
