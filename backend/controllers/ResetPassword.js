70a5e9a4386bfcdd66a080b3284a946384e33dcd
ram kumar <rammaniyari@gmail.com>
2023-09-17 04:26:38 +0530

    reset password controller

--- backend/controllers/ResetPassword.js
+++ backend/controllers/ResetPassword.js
@@ -52,3 +52,65 @@ exports.resetPasswordToken = async (req, res) => {
         })
     }
 }
+
+
+// reset password controller
+
+exports.resetPassword = async (req, res) => {
+    try {
+
+        // fetch data
+        const { password, confirmPassword, token } = req.body;
+        // validation
+        if (password !== confirmPassword) {
+            return res.status(403).json({
+                success: false,
+                message: "Password not matching",
+            })
+        }
+
+        // get userDetails from db using token
+        const userDetails = await User.findOne({ token: token });
+
+        // if no entry - invalid token
+
+        if (!userDetails) {
+            return res.status(403).json({
+                success: false,
+                message: "token is invalid",
+            })
+        }
+
+        // token time check
+
+        if (userDetails.resetPasswordExpires < Date.now()) {
+            return res.json({
+                success: false,
+                message: 'Token is expired, please regenerate your token',
+            });
+        }
+
+        //hash pwd
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //password update
+        await User.findOneAndUpdate(
+            { token: token },
+            { password: hashedPassword },
+            { new: true },
+        );
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: 'Password reset successful',
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Something went wrong while sending reset pwd mail'
+        })
+    }
+}
