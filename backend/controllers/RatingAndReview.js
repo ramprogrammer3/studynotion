d6fa39d5c0e23508097e815d89a9f8200c2a1b6d
ram kumar <rammaniyari@gmail.com>
2023-09-21 17:44:40 +0530

    get all rating and review

--- backend/controllers/RatingAndReview.js
+++ backend/controllers/RatingAndReview.js
@@ -83,7 +83,7 @@ exports.createRating = async (req, res) => {
 
 // get average Rating
 
-exports.getAverageRating = async(req,res)=>{
+exports.getAverageRating = async (req, res) => {
     try {
         // get course id
         const courseId = req.body.courseId;
@@ -91,44 +91,75 @@ exports.getAverageRating = async(req,res)=>{
 
         const result = await RatingAndReview.aggregate([
             {
-                $match : {
-                    course : new mongoose.Types.ObjectId(courseId);
+                $match: {
+                    course: new mongoose.Types.ObjectId(courseId),
                 },
 
             },
 
             {
-                $group :{
-                    _id : null,
-                    averageRating : {$avg : "$rating"},
+                $group: {
+                    _id: null,
+                    averageRating: { $avg: "$rating" },
                 }
             }
         ])
 
         // return rating
 
-        if(result.length > 0){
+        if (result.length > 0) {
             return res.status(200).json({
-                success : true,
-                averageRating : result[0].averageRating,
+                success: true,
+                averageRating: result[0].averageRating,
             })
         }
 
         // if no rating / review exist
 
         return res.status(200).json({
-            success : true,
-            message : "Average Rating is 0, no rating given till now",
-            averageRating : 0,
+            success: true,
+            message: "Average Rating is 0, no rating given till now",
+            averageRating: 0,
         })
 
 
     } catch (error) {
 
         return res.status(500).json({
-            success : false,
-            message : error.message,
+            success: false,
+            message: error.message,
         })
-        
+
+    }
+}
+
+// get all  rating and review
+
+exports.getAllRating = async (req, res) => {
+    try {
+        const allReviews = await RatingAndReview.find({})
+            .sort({ rating: "desc" })
+            .populate({
+                path: "user",
+                select: "firstName lastName email image",
+            })
+            .populate({
+                path: "course",
+                select: "courseName",
+            })
+            .exec();
+
+        return res.status(200).json({
+            success: true,
+            message: "All reviews fetched successfully",
+            data: allReviews,
+        })
+    } catch (error) {
+
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+
     }
 }
