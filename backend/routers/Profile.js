4a8aa1583692e16ec37c321f3c498bd0ac4c1b64
ram kumar <rammaniyari@gmail.com>
2023-09-24 07:58:05 +0530

    profile routes

---
+++ backend/routers/Profile.js
@@ -0,0 +1,22 @@
+// importing modules
+const express = require("express");
+const router = express.Router();
+
+const { auth } = require("../middlewares/auth");
+
+const { deleteAccount, updateProfile, getAllUserDetails, updateDisplayPicture, getEnrolledCourses } = require("../controllers/Profile")
+
+/*
+    ***********************************************************************************************************
+                                            Profile routes
+    ***********************************************************************************************************
+*/
+
+router.delete("/deleteProfile", deleteAccount);
+router.put("/updateProfle", auth, updateProfile);
+router.get("/getUserDetails", auth, getAllUserDetails);
+// get Enrolled course
+router.get("/getEnrolledCourses", auth, getEnrolledCourses);
+router.put("/updateDisplayPicture", auth, updateDisplayPicture);
+
+module.exports = router;
