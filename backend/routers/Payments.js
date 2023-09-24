a7781499c2e29a7fe7061846d1f7065f6acbcafa
ram kumar <rammaniyari@gmail.com>
2023-09-24 07:49:54 +0530

    payment router

---
+++ backend/routers/Payments.js
@@ -0,0 +1,12 @@
+// import the required modules
+const express = require("express");
+const router = express.Router();
+
+const { capturePayment, verifySignature } = require("../controllers/Payment");
+
+const { auth, isStudent, isInstructor, isAdmin } = require("../middlewares/auth");
+
+router.post("/capturePayment", auth, isStudent, capturePayment);
+router.post("/verifySignature", verifySignature);
+
+module.exports = router;
